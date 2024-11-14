327d691988c20800125beee3357d6a1e1a5a31cd
ManikanteshwaraReddy <22bd1a0529@gmail.com>
2024-11-14 15:50:24 +0530

    maven files added

---
+++ src/main/java/com/maven/demo/Maven_demo/App.java
@@ -0,0 +1,10 @@
+package com.maven.demo.Maven_demo;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Manikanteshwara Reddy" );
+        System.out.println("22BD1A0529");
+    }
+}
